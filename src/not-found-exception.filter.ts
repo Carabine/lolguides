51970380d520c2f0709d4da2a5501bb7e477aecd
unknown <busystrike@gmail.com>
2023-09-06 19:46:11 +0300

    added http

--- src/not-found-exception.filter.ts
+++ src/not-found-exception.filter.ts
@@ -5,7 +5,6 @@ export class NotFoundExceptionFilter implements ExceptionFilter {
   catch(_exception: NotFoundException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
-    console.log("404")
     response.redirect('/en/guides');
   }
 }
